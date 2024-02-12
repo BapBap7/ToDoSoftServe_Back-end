0d97fde52cfee37213f2af89401a4690ad015cb2
Dmytro <dimakhved@gmail.com>
2024-02-12 10:36:40 +0200

    update: endpoints links

--- Todo.API/obj/Debug/net8.0/Todo.API.AssemblyInfo.cs
+++ Todo.API/obj/Debug/net8.0/Todo.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0296f4ce84110f8e50f137646ab6d2890006748e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15eda02c62350badadf0297b128236fc3c66245")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
